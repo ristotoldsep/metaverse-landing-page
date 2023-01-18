6beb39cc29bdd4df88175e707cc98a89d40a7de9
Risto Tõldsep <r.toldsep@gmail.com>
2023-01-18 19:07:27 +0200

    Finished new features  section

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -13,11 +13,11 @@ const GetStarted = () => (
       variants={staggerContainer}
       initial="hidden"
       whileInView="show"
-      viewport={{once: 'false', amount: 0.25}}
+      viewport={{ once: "false", amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
     >
       <motion.div
-        variants={planetVariants('left')}
+        variants={planetVariants("left")}
         className={`flex-1 ${styles.flexCenter}`}
       >
         <img
@@ -27,22 +27,17 @@ const GetStarted = () => (
         />
       </motion.div>
       <motion.div
-        variants={fadeIn('left', 'tween', 0.2, 1)}
+        variants={fadeIn("left", "tween", 0.2, 1)}
         className="flex-[0.75] flex justify-center flex-col"
       >
         <TypingText title="| How Metaversus Works" />
         <TitleText title={<>Get started with just a few clicks</>} />
-        <div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px]">
+        <div className="mt-[31px] flex flex-wrap flex-col max-w-[370px] gap-[24px]">
           {startingFeatures.map((feature, index) => (
-            <StartSteps
-              key={feature}
-              number={index + 1}
-              text={feature}
-            />
+            <StartSteps key={feature} number={index + 1} text={feature} />
           ))}
         </div>
       </motion.div>
-
     </motion.div>
   </section>
 );
