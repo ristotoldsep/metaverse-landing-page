a5d323c2320585698458c0239c5e3f417cf666af
Risto Tõldsep <r.toldsep@gmail.com>
2023-01-19 03:12:17 +0200

    Finished insights  section

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -21,7 +21,7 @@ const WhatsNew = () => (
         className="flex-[0.75] flex justify-center flex-col"
       >
         <TypingText title="| What's New?" />
-        <TitleText title={<>What's new about Metaversus?</>} />
+        <TitleText title={<>What's new about Metaversus?</>} textStyles="md:text-[58px]" />
         <div className="mt-[48px] flex flex-wrap justify-between gap-[24px]">
           {newFeatures.map((feature) => (
             <NewFeatures key={feature.title} {...feature} />
