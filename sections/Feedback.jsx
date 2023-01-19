1249673ae894533110838e9f56d2deb9f16b9b78
Risto Tõldsep <r.toldsep@gmail.com>
2023-01-19 18:26:11 +0200

    Finished footer section

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -19,20 +19,23 @@ const Feedback = () => (
         className="flex-[0.5] lg:max-w-[370px] flex justify-end flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6a6a6a] relative"
       >
         <div className="feedback-gradient" />
-          <div className="">
-            <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40px] leading-[36px] text-white">
-              Rico
-            </h4>
-            <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22px] leading-[16px] text-white">
-              Founder | Metaversus
-            </p>
-            <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45px] leading-[39px] text-white">
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nobis ipsum officia vel! Sunt ducimus at cupiditate blanditiis a? Eum illum odio dolorum eius, delectus neque tenetur iusto accusantium laudantium."
-            </p>
-          </div>
+        <div className="">
+          <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40px] leading-[36px] text-white">
+            Rico
+          </h4>
+          <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22px] leading-[16px] text-white">
+            Founder | Metaversus
+          </p>
+          <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45px] leading-[39px] text-white">
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+            nobis ipsum officia vel! Sunt ducimus at cupiditate blanditiis a?
+            Eum illum odio dolorum eius, delectus neque tenetur iusto
+            accusantium laudantium."
+          </p>
+        </div>
       </motion.div>
       <motion.div
-        variants={fadeIn('left', 'tween', 0.2, 1)}
+        variants={fadeIn("left", "tween", 0.2, 1)}
         className="relative flex-1 flex justify-center items-center"
       >
         <img
@@ -42,7 +45,7 @@ const Feedback = () => (
         />
         <motion.div
           variants={zoomIn(0.4, 1)}
-          className="lg:block hidden absolute -left-[10%] top-[3%]"
+          className="lg:block absolute lg:-left-[10%] lg:top-[3%] -left-[0%] top-[3%]"
         >
           <img
             src="/stamp.png"
